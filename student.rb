0302f189a2476311ccf0e46581e91cd7fd9da164
Jesse Wolgamott <jesse@comalproductions.com>
2013-07-01 13:51:10 -0500

    Initial Add: Super Cool Seniors

---
+++ student.rb
@@ -0,0 +1,30 @@
+class Student
+
+	attr_reader :first_name, :last_name, :grade
+	def initialize(first_name, last_name, grade)
+		@first_name = first_name
+		@last_name = last_name
+		@grade = grade
+	end
+
+	def senior?
+		grade == 12
+	end
+
+	def to_s
+		"#{last_name}, #{first_name}"
+	end
+end
+
+def seniors(students)
+	students.select { |student| student.senior? }
+end
+
+fred = Student.new("Fred", "James", 12)
+sarah = Student.new("Sarah", "Smith", 12)
+jack = Student.new("Jack", "Gong", 10)
+all_students = [fred, sarah, jack]
+
+seniors(all_students).each do |student|
+	puts student
+end
